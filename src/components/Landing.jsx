27de6387cde7e20dbf1e7928cef8aaa5f30525b9
Osman <osman.cakir1@outlook.com>
2023-03-16 01:06:03 +1100

    added slider

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css";
 
 function Landing() {
   const [posters, setPosters] = useState([]);
-
   async function fetchPosters() {
     const response = await axios.get(
       "https://api.themoviedb.org/3/trending/movie/day?api_key=b361eac269c6fa0e8314d430f2779f8e"
@@ -19,16 +20,24 @@ function Landing() {
   console.log(posters);
 
   return (
-    <>
-      {posters.slice(0, 4).map((poster) => (
-        <div className="container">
-          <div className="row">{poster.title}</div>
-            <img src={`https://image.tmdb.org/t/p/w500${
-          poster.backdrop_path || poster.poster_path
-        }`} alt="" />
+    <section id="slider">
+      <div className="container">
+        <div className="row">
+          <Slide easing="ease">
+            {posters.slice(0, 4).map((poster) => (
+              <div className="poster__slider">
+                <img
+                  src={`https://image.tmdb.org/t/p/w500${
+                    poster.backdrop_path || poster.poster_path
+                  }`}
+                  alt={poster.title}
+                />
+              </div>
+            ))}
+          </Slide>
         </div>
-      ))}
-    </>
+      </div>
+    </section>
   );
 }
 
